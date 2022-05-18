35fb4185908814824c9e8ef159725446c641baa5
mndream <910536551@qq.com>
2022-05-19 00:43:46 +0800

    Add func for print data table rows.

--- mk_html.py
+++ mk_html.py
@@ -3,14 +3,28 @@
 import os
 import dominate
 from dominate.tags import *
+import json
 
 js_str = ''
 
 
-def create_html(html_file_path='./timeline_chart.html'):
+def print_rows(data_dict_arr=[]):
+    """  [ 'Washington', new Date(1789, 3, 30), new Date(1797, 2, 4) ],"""
+    rows_str = ''
+    for data in data_dict_arr:
+        row_str = "['"
+        row_str += str(data['pkg']) + "',"
+        row_str += str(data['start']) + ','
+        row_str += str(data['end']) + '],'
+        rows_str += row_str
+    return rows_str
+
+
+def create_html(html_file_path='./timeline_chart.html', json_data=""):
     """
-    根据 data.json 构建 HTML 页面
-    :param html_file_path: HTML 页面输出位置
+    Create HTML by json_data.
+    :param html_file_path: HTML file path
+    :param json_data: source data
     :return: None
     """
     doc = dominate.document(title='LogShow')
@@ -37,15 +51,15 @@ def create_html(html_file_path='./timeline_chart.html'):
     #     js = f.read()
 
     append_js_str("""
-  google.charts.load("current", {packages:["timeline","controls"],'language': 'ja'});
-  google.charts.setOnLoadCallback(drawChart);
-  
-  function drawChart() {
-  var dashboard = new google.visualization.Dashboard(
-    document.getElementById('dashboard')
-  );
+    google.charts.load("current", {packages:["timeline","controls"],'language': 'ja'});
+    google.charts.setOnLoadCallback(drawChart);
 
-  var control = new google.visualization.ControlWrapper({
+    function drawChart() {
+    var dashboard = new google.visualization.Dashboard(
+        document.getElementById('dashboard')
+    );
+
+    var control = new google.visualization.ControlWrapper({
     controlType: 'ChartRangeFilter',
     containerId: 'control',
     options: {
@@ -64,12 +78,10 @@ def create_html(html_file_path='./timeline_chart.html'):
             baselineColor: 'none'
           }
         },
-        chartView: {
-          columns: [1, 2]
+        chartView: {columns: [1, 2]}
         }
       }
-    }
-  });
+    });
 
   var chart = new google.visualization.ChartWrapper({
     chartType: 'Timeline',
@@ -90,58 +102,23 @@ def create_html(html_file_path='./timeline_chart.html'):
     }
   });
 
-  var dataTable = new google.visualization.DataTable();
-  dataTable.addColumn({ type: 'string', id: 'President' });
-  dataTable.addColumn({ type: 'date', id: 'Start' });
-  dataTable.addColumn({ type: 'date', id: 'End' });
-  dataTable.addRows([
-  [ 'Washington', new Date(1789, 3, 30), new Date(1797, 2, 4) ],
-  [ 'Adams',      new Date(1797, 2, 4),  new Date(1801, 2, 4) ],
-  [ 'Jefferson',  new Date(1801, 2, 4),  new Date(1809, 2, 4) ],
-    [ 'Washington', new Date(1789, 3, 30), new Date(1797, 2, 4) ],
-  [ 'Adams',      new Date(1797, 2, 4),  new Date(1801, 2, 4) ],
-  [ 'Jefferson',  new Date(1801, 2, 4),  new Date(1809, 2, 4) ],
-    [ 'Washington', new Date(1789, 3, 30), new Date(1797, 2, 4) ],
-  [ 'Adams',      new Date(1797, 2, 4),  new Date(1801, 2, 4) ],
-  [ 'Jefferson',  new Date(1801, 2, 4),  new Date(1809, 2, 4) ],
-    [ 'Washington', new Date(1789, 3, 30), new Date(1797, 2, 4) ],
-  [ 'Adams',      new Date(1797, 2, 4),  new Date(1801, 2, 4) ],
-  [ 'Jefferson',  new Date(1801, 2, 4),  new Date(1809, 2, 4) ],
-    [ 'Washington', new Date(1789, 3, 30), new Date(1797, 2, 4) ],
-  [ 'Adams',      new Date(1797, 2, 4),  new Date(1801, 2, 4) ],
-  [ 'Jefferson',  new Date(1801, 2, 4),  new Date(1809, 2, 4) ],
-    [ 'Washington', new Date(1789, 3, 30), new Date(1797, 2, 4) ],
-  [ 'Adams',      new Date(1797, 2, 4),  new Date(1801, 2, 4) ],
-  [ 'Jefferson',  new Date(1801, 2, 4),  new Date(1809, 2, 4) ],
-    [ 'Washington', new Date(1789, 3, 30), new Date(1797, 2, 4) ],
-  [ 'Adams',      new Date(1797, 2, 4),  new Date(1801, 2, 4) ],
-  [ 'Jefferson',  new Date(1801, 2, 4),  new Date(1809, 2, 4) ],
-    [ 'Washington', new Date(1789, 3, 30), new Date(1797, 2, 4) ],
-  [ 'Adams',      new Date(1797, 2, 4),  new Date(1801, 2, 4) ],
-  [ 'Jefferson',  new Date(1801, 2, 4),  new Date(1809, 2, 4) ],
-    [ 'Washington', new Date(1789, 3, 30), new Date(1797, 2, 4) ],
-  [ 'Adams',      new Date(1797, 2, 4),  new Date(1801, 2, 4) ],
-  [ 'Jefferson',  new Date(1801, 2, 4),  new Date(1809, 2, 4) ],
-    [ 'Washington', new Date(1789, 3, 30), new Date(1797, 2, 4) ],
-  [ 'Adams',      new Date(1797, 2, 4),  new Date(1801, 2, 4) ],
-  [ 'Jefferson',  new Date(1801, 2, 4),  new Date(1809, 2, 4) ],
-    [ 'Washington', new Date(1789, 3, 30), new Date(1797, 2, 4) ],
-  [ 'Adams',      new Date(1797, 2, 4),  new Date(1801, 2, 4) ],
-  [ 'Jefferson',  new Date(1801, 2, 4),  new Date(1809, 2, 4) ],
-    [ 'Washington', new Date(1789, 3, 30), new Date(1797, 2, 4) ],
-  [ 'Adams',      new Date(1797, 2, 4),  new Date(1801, 2, 4) ],
-  [ 'Jefferson',  new Date(1801, 2, 4),  new Date(1809, 2, 4) ],
-    [ 'Washington', new Date(1789, 3, 30), new Date(1797, 2, 4) ],
-  [ 'Adams',      new Date(1797, 2, 4),  new Date(1801, 2, 4) ],
-  [ 'Jefferson',  new Date(1801, 2, 4),  new Date(1809, 2, 4) ],
-    [ 'Washington', new Date(1789, 3, 30), new Date(1797, 2, 4) ],
-  [ 'Adams',      new Date(1797, 2, 4),  new Date(1801, 2, 4) ],
-  [ 'Jefferson',  new Date(1801, 2, 4),  new Date(1809, 2, 4) ],
-  ]);
-
-  dashboard.bind(control, chart);
-  dashboard.draw(dataTable);
-}
+    var dataTable = new google.visualization.DataTable();
+    dataTable.addColumn({ type: 'string', id: 'pkg' });
+    dataTable.addColumn({ type: 'date', id: 'Start' });
+    dataTable.addColumn({ type: 'date', id: 'End' });
+    dataTable.addRows([""")
+    # TODO: get real data and convert to data_dict_arr
+    data = {
+        'pkg': 'com.baidu',
+        'start': ms_str_2_date("200"),
+        'end': ms_str_2_date("300"),
+    }
+    data_dict_arr = [data]
+    append_js_str(print_rows(data_dict_arr))
+    append_js_str("""]);
+    dashboard.bind(control, chart);
+    dashboard.draw(dataTable);
+    }
     """)
 
     with doc:
@@ -165,6 +142,15 @@ def create_html(html_file_path='./timeline_chart.html'):
         f.write(doc_str)
 
     os.startfile(os.path.abspath(html_file_path))
+
+
+def ms_str_2_date(ms_str):
+    """
+    Convert timeMs to js Date.
+    :param ms_str: timeMs string.
+    :return: js Date str.
+    """
+    return "new Date(%s * 1000)" % ms_str
 
 
 def append_js_str(js_append):
